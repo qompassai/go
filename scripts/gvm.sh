d53dbb5e49dbd153a2deed3221fd01113075039f
phaedrusflow <map@qompass.ai>
2025-11-25 17:21:38 -0800

    Update 2025-11-25

--- scripts/gvm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-# gvm.sh
-# Qompass AI - [Add description here]
-# Copyright (C) 2025 Qompass AI, All rights reserved
-# ----------------------------------------
-gvm install go1.24.6
